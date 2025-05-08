4dac9d2453c1880c784437129a2a185baa92e358
soethiha <usersoe123@gmail.com>
2025-05-08 19:16:03 +0630

    changing icons

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,11 +2,12 @@
 import React, {useState} from 'react'
 import Image from "next/image";
 import Link from 'next/link';
+
 import { Search, Home, Bell, Video, User, Store, Users, MessageCircle, Settings } from 'lucide-react';
 
 const navbarItemsData = [
-  { icon: Home, url: "/" },
-  { icon: Bell, url: "/notifications" },
+  { icon: HomeICon, url: "/" },
+  { icon: VideoIcon, url: "/notifications" },
   { icon: Video, url: "/watch" },
   { icon: Users, url: "/friends" },
 ];
@@ -33,11 +34,11 @@ export default function Navbar() {
   return ( 
     <nav className="navbar">
         <div className="logo-container h-10/12 flex  rounded-2xl mx-6">
-          <Image className='mr-3' src="/facebook-logo.png" width="50" height="50" alt="facebook logo"/>
+          {/* <Image className='mr-3' src="/facebook-logo.png" width="50" height="50" alt="facebook logo"/> */}
+          <Image className='mr-3' src="/2023_Facebook_icon.svg" width="50" height="50" alt="facebook logo"/>
           <div className='input-container flex flex-row items-center justify-start bg-gray-200 mr-4  w-56 rounded-full'>
             <Search className='absolute m-2'/>
             <input className='border-0 w-/12 h-10/12 ml-8 rounded-full focus:outline-0 pl-2 caret-transparent' type='text' placeholder="search facebook"></input>
-
           </div>
         </div>
         <ul className="navbar-items-container w-4/12 mr-6/12 ml-[10%]">
@@ -57,5 +58,21 @@ export default function Navbar() {
           <li className="navbar-item"><button><User/></button></li>
         </ul>
       </nav>
+  )
+}
+
+export function HomeICon() {
+  return(
+    <>
+      <svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor" className="pxfx01vb px1lliihq px1tzjh5l px1k90msu px2h7rmj px1qfuztq hico" ><path d="M9.464 1.286C10.294.803 11.092.5 12 .5c.908 0 1.707.303 2.537.786.795.462 1.7 1.142 2.815 1.977l2.232 1.675c1.391 1.042 2.359 1.766 2.888 2.826.53 1.059.53 2.268.528 4.006v4.3c0 1.355 0 2.471-.119 3.355-.124.928-.396 1.747-1.052 2.403-.657.657-1.476.928-2.404 1.053-.884.119-2 .119-3.354.119H7.93c-1.354 0-2.471 0-3.355-.119-.928-.125-1.747-.396-2.403-1.053-.656-.656-.928-1.475-1.053-2.403C1 18.541 1 17.425 1 16.07v-4.3c0-1.738-.002-2.947.528-4.006.53-1.06 1.497-1.784 2.888-2.826L6.65 3.263c1.114-.835 2.02-1.515 2.815-1.977zM10.5 13A1.5 1.5 0 0 0 9 14.5V21h6v-6.5a1.5 1.5 0 0 0-1.5-1.5h-3z"></path></svg>
+    </>
+  )
+}
+
+export function VideoIcon() {
+  return(
+    <>
+      <svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor" className="xfx01vb x1lliihq x1tzjh5l x1k90msu x2h7rmj x1qfuztq"><path d="M10.996 8.132A1 1 0 0 0 9.5 9v4a1 1 0 0 0 1.496.868l3.5-2a1 1 0 0 0 0-1.736l-3.5-2z"></path><path d="M14.573 2H9.427c-1.824 0-3.293 0-4.45.155-1.2.162-2.21.507-3.013 1.31C1.162 4.266.817 5.277.655 6.477.5 7.634.5 9.103.5 10.927v.146c0 1.824 0 3.293.155 4.45.162 1.2.507 2.21 1.31 3.012.802.803 1.813 1.148 3.013 1.31C6.134 20 7.603 20 9.427 20h5.146c1.824 0 3.293 0 4.45-.155 1.2-.162 2.21-.507 3.012-1.31.803-.802 1.148-1.813 1.31-3.013.155-1.156.155-2.625.155-4.449v-.146c0-1.824 0-3.293-.155-4.45-.162-1.2-.507-2.21-1.31-3.013-.802-.802-1.813-1.147-3.013-1.309C17.866 2 16.397 2 14.573 2zM3.38 4.879c.369-.37.887-.61 1.865-.741C6.251 4.002 7.586 4 9.5 4h5c1.914 0 3.249.002 4.256.138.978.131 1.496.372 1.865.74.37.37.61.888.742 1.866.135 1.007.137 2.342.137 4.256 0 1.914-.002 3.249-.137 4.256-.132.978-.373 1.496-.742 1.865-.369.37-.887.61-1.865.742-1.007.135-2.342.137-4.256.137h-5c-1.914 0-3.249-.002-4.256-.137-.978-.132-1.496-.373-1.865-.742-.37-.369-.61-.887-.741-1.865C2.502 14.249 2.5 12.914 2.5 11c0-1.914.002-3.249.138-4.256.131-.978.372-1.496.74-1.865zM8 21.5a1 1 0 1 0 0 2h8a1 1 0 1 0 0-2H8z"></path></svg>
+    </>
   )
 }
--- app/components/StatusBox.tsx
+++ app/components/StatusBox.tsx
@@ -16,30 +16,38 @@ export default function StatusBox() {
 
 
 
-
   return (
     <>
     <div className='status-box w-full shadow-md mt-8 rounded-2xl'>
-      <div className='first-section p-4 flex flex-row items-center'>
-        <Link href="#">
-          <User className='w-8 h-8 rounded-4xl border-1 mx-3'/>
-        </Link>
+      <div className='first-section p-1 flex flex-row items-center'>
+        
+          {/* <User className='w-8 h-8 rounded-4xl border-1 mx-3'/> */}
+        <img className='w-10 h-10 rounded-4x1 border-1 mx-2 pfp' src="/mypfp.jpg"></img>
         <button onClick={ToggleUploadModal} className='bg-gray-100 rounded-3xl w-full text-start px-4 h-8'>What's on your mind , User? </button>
       </div>
         <hr className='w-11/12 mx-auto text-gray-300'></hr>
       <div className='second-section flex flex-row justify-center'>
         <div className="status-activity">
-          <Video/> <p>Live Video</p>
+          <img width="24" height="24" src="/fb-video-icon.png"></img><p>Live Video</p>
         </div>
         <div className="status-activity">
-          <Image/><p>Photo/video</p>
+        <img width="24" height="24" src="/fb-image-icon.png"></img><p>Photo/video</p>
         </div>
         <div className="status-activity">
-          <Smile/><p>Feeling/activity</p>
+        <img width="24" height="24" src="/fb-smile-icon.png"/><p>Feeling/activity</p>
         </div>
       </div>
     </div>
       {showUploadModal && <PostUploadModal toggleUploadModal={ToggleUploadModal}/>}
+    </>
+  )
+}
+
+
+export function PfIcon() {
+  return (
+    <>
+      {/* <a aria-label="Soe Thi Ha's Timeline" class="x1i10hfl x1qjc9v5 xjbqb8w xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xdl72j9 x2lah0s xe8uvvx xdj266r xat24cr x1mh8g0r x2lwn1j xeuugli xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x1q0g3np x87ps6o x1lku1pv x1a2a7pz x78zum5 x1emribx" href="https://www.facebook.com/profile.php?id=100077793423530" role="link" tabindex="0"><div class="html-div xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd"><div class="x1rg5ohu x1n2onr6 x3ajldb x1ja2u2z"><svg aria-hidden="true" class="x3ajldb" data-visualcompletion="ignore-dynamic" role="none" style="height: 40px; width: 40px;"><mask id="«r26o»"><circle cx="20" cy="20" fill="white" r="20"></circle></mask><g mask="url(#«r26o»)"><image x="0" y="0" height="100%" preserveAspectRatio="xMidYMid slice" width="100%" xlink:href="https://scontent.xx.fbcdn.net/v/t39.30808-1/494725964_703116392291469_4066282531775240103_n.jpg?stp=c769.0.1080.1080a_cp0_dst-jpg_s40x40_tt6&amp;_nc_cat=105&amp;ccb=1-7&amp;_nc_sid=e99d92&amp;_nc_ohc=0SKCRVx0fQwQ7kNvwFhHokx&amp;_nc_oc=AdkZR0yIZUUr1RomBqWcpt_XJbdc_zFSWNsttojxIi5vqkgE5AWlzgLS5T8RsLMSbFU&amp;_nc_zt=24&amp;_nc_ht=scontent.xx&amp;_nc_gid=oSfmVVlUxvOoWQ2lRJzw1Q&amp;oh=00_AfKiq23Uxts4sj9wawZjqyseZMdDbWdMGtBkEVtiegBjrg&amp;oe=6822544C" style="height: 40px; width: 40px;"></image><circle class="xbh8q5q x1pwv2dq xvlca1e" cx="20" cy="20" r="20"></circle></g></svg></div></div><div class="x1ey2m1c xds687c x17qophe xg01cxk x47corl x10l6tqk x13vifvy x1ebt8du x19991ni x1dhq9h xzolkzo x12go9s9 x1rnf11y xprq8jg" role="none" data-visualcompletion="ignore" style="inset: 0px;"></div></a> */}
     </>
   )
 }
